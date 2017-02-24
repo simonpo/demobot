9c53ffd3af73d7fd537803c6d69f533e9424acf4
Simon Powell <simonpo@microsoft.com>
2017-02-23 17:19:02 -0800

    Updated bot responses

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ server.post('/api/messages', connector.listen());
 
 // Create bot dialogs
 bot.dialog('/', function (session) {
-    session.send("Hello World");
+    session.send("Hello Everybody");
 });
 
 // web interface
